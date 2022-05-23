7e7b2b3593166db39547aea8ec91bb971b0175e3
Aayush Garg <aayushgarg2006@gmail.com>
2022-05-23 17:10:50 +0530

    Update Views And Routers:Chores
    1. Add Register Page, working on others

--- Routers/index_router.js
+++ Routers/index_router.js
@@ -1,9 +1,28 @@
 //Express
-const express = require('express');
+const express = require("express");
 const IndexRouter = express.Router();
 
-IndexRouter.get('/', (req, res)=>{
-    res.render('index')
-})
+events = [
+    "AppD",
+    "WebD",
+    "2D design",
+    "UI Design",
+    "3D Design",
+    "A / V editing",
+    "Quiz",
+    "Crossword",
+    "Cryptic Hunt",
+    "Gaming",
+    "Competitive Programming",
+    "Photography",
+    "Audio mixing",
+    "Film Making",
+    "GD",
+    "Hardware",
+];
+
+IndexRouter.get("/", (req, res) => {
+    res.render("index", {events});
+});
 
 module.exports = IndexRouter;
