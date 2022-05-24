f4ac7e36adf7ec0da7515bbfc095155c4b68d74c
Aayush Garg <aayushgarg2006@gmail.com>
2022-05-24 18:03:55 +0530

    Added Node Mailer, Chores: Completed backend
    1. Add node mailer
    2. setup validation
    3. complete frontend
    4. support phones

--- Routers/index_router.js
+++ Routers/index_router.js
@@ -1,8 +1,20 @@
 //Express
 const express = require("express");
 const IndexRouter = express.Router();
+const ejs = require("ejs");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const { v4: uuidv4 } = require('uuid');
+const { Client } = require("@notionhq/client")
+const nodemailer = require('nodemailer');
 
-events = [[
+// Initializing a client
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+})
+const databaseId = process.env.NOTION_DATABASE_ID
+
+const events = [[
     "App Development",
     "Web Development",
     "2D design",
@@ -28,7 +40,210 @@ IndexRouter.get("/", (req, res) => {
     res.render("index", { events });
 });
 
-IndexRouter.post("/register", (req, res) => {
+// Transport To Send Mail
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    secureConnection: false,
+    port: 465,
+    auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_PASSWORD // naturally, replace both with your real credentials or an application-specific password
+    }, tls: {
+        rejectUnauthorized: false
+    }
 });
+
+IndexRouter.post('/register', async (req, res) => {
+    var { name, dob, email, phone, adno, grade, section, disname, pasw, selected } = await req.body;
+
+    if (!(name && dob && email && phone && adno && grade && section && disname && pasw && selected)) {
+        return res.status(400).send("Please fill all the fields");
+    }
+    if (new Date(dob).getFullYear() < 2000) {
+        return res.status(400).send("Please enter a valid date of birth");
+    }
+    if (phone.length != 10) {
+        return res.status(400).send("Please enter a valid phone number");
+    }
+    // email regex
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(email)) {
+        return res.status(400).send("Please enter a valid email");
+    }
+    if (adno.length != 4) {
+        return res.status(400).send("Please enter a valid admission number");
+    }
+    if (grade < 1 || grade > 12) {
+        return res.status(400).send("Please enter a valid class");
+    }
+
+    const dis_token = uuidv4();
+    pasw = await bcrypt.hash(pasw, saltRounds);
+
+    var res1 = await addItem(name, dob, email, phone, adno, grade, section, disname, pasw, selected, dis_token);
+    console.log(res1);
+
+    const mailOptions = {
+        from: process.env.GMAIL_EMAIL,
+        to: email,
+        subject: 'InTech Registeration Details',
+        html: res.send(await renderFile('./views/email.ejs', { age: getAge(new Date("2006-02-24")), dis_token, name }))
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            res.send({ success: false, error });
+        } else {
+            res.send({ success: true, msg: 'email-send', info });
+        }
+    });
+});
+
+function getAge(birthDate) {
+    var now = new Date();
+
+    function isLeap(year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
+
+    // days since the birthdate    
+    var days = Math.floor((now.getTime() - birthDate.getTime()) / 1000 / 60 / 60 / 24);
+    var age = 0;
+    // iterate the years
+    for (var y = birthDate.getFullYear(); y <= now.getFullYear(); y++) {
+        var daysInYear = isLeap(y) ? 366 : 365;
+        if (days >= daysInYear) {
+            days -= daysInYear;
+            age++;
+            // increment the age only if there are available enough days for the year.
+        }
+    }
+    return age;
+}
+
+const renderFile = (file, data) => {
+    return new Promise(resolve => {
+        ejs.renderFile(file, data, (err, result) => {
+            if (err) {
+                logger.error(err);
+            }
+            resolve(result);
+        });
+    });
+}
+
+
+async function addItem(name, dob, email, phone, adno, grade, section, disname, pasw, selected, dis_token) {
+    try {
+        const response = await notion.pages.create({
+            parent: { database_id: databaseId },
+            properties: {
+                "Name": {
+                    title: [
+                        {
+                            "text": {
+                                "content": name
+                            }
+                        }
+                    ]
+                },
+                "Email": {
+                    "type": "rich_text",
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": { "content": email }
+                        }
+                    ]
+                },
+                "Phone": {
+                    "type": "rich_text",
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": { "content": phone }
+                        }
+                    ]
+                },
+                "Password": {
+                    "type": "rich_text",
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": { "content": pasw }
+                        }
+                    ]
+                },
+                "Admission Number": {
+                    "type": "rich_text",
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": { "content": adno }
+                        }
+                    ]
+                },
+                "DOB": {
+                    "type": "rich_text",
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": { "content": dob }
+                        }
+                    ]
+                },
+                "grade": {
+                    "type": "rich_text",
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": { "content": grade }
+                        }
+                    ]
+                },
+                "section": {
+                    "type": "rich_text",
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": { "content": section }
+                        }
+                    ]
+                },
+                "DisplayName": {
+                    "type": "rich_text",
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": { "content": disname }
+                        }
+                    ]
+                },
+                "Fields": {
+                    "type": "multi_select",
+                    "multi_select":
+                        selected.map(x => {
+                            return {
+                                "name": x
+                            }
+                        })
+                },
+                "Discord Secret": {
+                    "type": "rich_text",
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": { "content": dis_token }
+                        }
+                    ]
+                }
+            },
+        })
+        return response;
+    } catch (error) {
+        return error;
+    }
+}
 
 module.exports = IndexRouter;
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 //Discord
 const { Client, Intents } = require('discord.js');
@@ -28,14 +29,17 @@ const client = new Client({
 });
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 app.use('/', IndexRouter)
 
 client.once('ready', c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}!`);
-    });
 });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
+
 
 //On a message call the on_message function
 client.on("messageCreate", message => {
