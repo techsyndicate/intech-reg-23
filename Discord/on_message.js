ea6ff87479d4033ff7d578d1d79c203c537d720d
Aayush Garg <aayushgarg2006@gmail.com>
2022-05-24 22:00:26 +0530

    UPdate dc bot

--- Discord/on_message.js
+++ Discord/on_message.js
@@ -1,25 +1,93 @@
-module.exports = (client, message) => {
+const { Client } = require("@notionhq/client");
+
+// Initializing a client
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+})
+const databaseId = process.env.NOTION_DATABASE_ID
+
+
+module.exports = async (client, message) => {
     if (!message.author.bot) {
         if (message.content.startsWith('ts ')) {
             message.content = message.content.slice(3);
             if (message.content.length > 0) {
                 command(client, message);
-                console.log(message.content);
             }
         }
     };
 }
 
-function command(client, message) {
+async function command(client, message) {
     if (message.content.startsWith('verify')) {
-        console.log(message.content)
         token = message.content.slice(7);
         if (token.length == 0) {
-            message.react('❌');
+            await message.react('❌');
+            await message.author.send('Please use a valid token!');
+            await message.delete();
             return;
         }
-        //dm the user that they are verified
-        message.react('✅');
-        message.author.send('You are verified!');
+
+        var response = await notion.databases.query({
+            database_id: databaseId,
+            filter: {
+                or: [
+                    {
+                        "property": 'Discord Secret',
+                        "rich_text": {
+                            "contains": token
+                        }
+                    }
+                ]
+            }
+        });
+        console.log(JSON.parse(JSON.stringify(response)));
+        if (!response.results.length>0) {
+            await message.react('❌');
+            await message.author.send('Invalid token!');
+            await message.delete();
+            return;
+        }
+        var properties = (response.results[0].properties);
+
+        try {
+            await message.member.setNickname(properties.DisplayName.rich_text[0].text.content);
+        } catch (error) {
+            console.log(error)
+        }
+        if (getAge(new Date(properties.DOB.rich_text[0].text.content)) >= 13) {
+            let role = await message.guild.roles.cache.find(r => r.name == 'participant')
+            await message.react('✅');
+            await message.author.send('You are verified!');
+            await message.member.roles.add(role);
+        }
+        else {
+            console.log(getAge(new Date(properties.DOB.rich_text[0].text.content)))
+            await message.react('❌');
+            await message.author.send('You are not 13 years old! Please follow discord tos!');
+        }
+        await message.delete();
     }
+}
+
+function getAge(birthDate) {
+    var now = new Date();
+
+    function isLeap(year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
+
+    // days since the birthdate    
+    var days = Math.floor((now.getTime() - birthDate.getTime()) / 1000 / 60 / 60 / 24);
+    var age = 0;
+    // iterate the years
+    for (var y = birthDate.getFullYear(); y <= now.getFullYear(); y++) {
+        var daysInYear = isLeap(y) ? 366 : 365;
+        if (days >= daysInYear) {
+            days -= daysInYear;
+            age++;
+            // increment the age only if there are available enough days for the year.
+        }
+    }
+    return age;
 }
